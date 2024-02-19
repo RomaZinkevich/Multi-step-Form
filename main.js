24621742cc2c2c41f0358c084560dc14f5cf34e6
RomaZinkevich <zra1903@mail.ru>
2024-02-19 16:41:02 +0200

    added function to go back in form

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 const plans = document.querySelectorAll(".plans .plan");
 const nextBtns = document.querySelectorAll("button.next");
+const backBtns = document.querySelectorAll("button.back");
 const forms = document.querySelectorAll(".form");
 const plansArray = Array.from(plans);
 const nextBtnsArray = Array.from(nextBtns);
+const backBtnsArray = Array.from(backBtns);
 
 plansArray.forEach((el, index) => {
     el.addEventListener("click", () => {
@@ -17,6 +19,14 @@ nextBtnsArray.forEach((el, index) => {
     })
 });
 
+backBtnsArray.forEach((el, index) => {
+    el.addEventListener("click", (event) => {
+        event.preventDefault();
+        prevStep(index);
+    })
+});
+
+
 function changePlan(index) {
     document.querySelector(".plan.active").classList.toggle("active");
     plans[index].classList.toggle("active");
@@ -25,6 +35,14 @@ function changePlan(index) {
 function nextStep(index) {
     document.querySelector(".form.active").classList.toggle("active");
     forms[index+1].classList.toggle("active");
-    document.querySelector(`.road .number`).classList.toggle("selected");
+    document.querySelector(`.road .number.selected`).classList.toggle("selected");
     document.querySelector(`.road.step-${index+2} .number`).classList.toggle("selected");
+}
+
+function prevStep(index) {
+    console.log(index)
+    document.querySelector(".form.active").classList.toggle("active");
+    forms[index].classList.toggle("active");
+    document.querySelector(`.road .number.selected`).classList.toggle("selected");
+    document.querySelector(`.road.step-${index+1} .number`).classList.toggle("selected");
 }
