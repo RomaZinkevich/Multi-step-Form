378e95f037d74b818566db06dff2a8c497c3ccd5
RomaZinkevich <zra1903@mail.ru>
2024-02-19 16:37:25 +0200

    added function to switch between steps

--- main.js
+++ main.js
@@ -1,13 +1,30 @@
 const plans = document.querySelectorAll(".plans .plan");
+const nextBtns = document.querySelectorAll("button.next");
+const forms = document.querySelectorAll(".form");
 const plansArray = Array.from(plans);
+const nextBtnsArray = Array.from(nextBtns);
 
 plansArray.forEach((el, index) => {
     el.addEventListener("click", () => {
         changePlan(index);
     })
-})
+});
+
+nextBtnsArray.forEach((el, index) => {
+    el.addEventListener("click", (event) => {
+        event.preventDefault();
+        nextStep(index);
+    })
+});
 
 function changePlan(index) {
     document.querySelector(".plan.active").classList.toggle("active");
     plans[index].classList.toggle("active");
+}
+
+function nextStep(index) {
+    document.querySelector(".form.active").classList.toggle("active");
+    forms[index+1].classList.toggle("active");
+    document.querySelector(`.road .number`).classList.toggle("selected");
+    document.querySelector(`.road.step-${index+2} .number`).classList.toggle("selected");
 }
